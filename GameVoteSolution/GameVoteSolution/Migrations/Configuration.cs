001090571439f9a5a05daab39bb75ba6927bf086
AB244 <ajbenn04@louisville.edu>
2014-04-21 19:10:15 -0400

    Roles implemented BUGGED
    Significant bugs exist. Debugging initiated.

--- GameVoteSolution/GameVoteSolution/Migrations/Configuration.cs
+++ GameVoteSolution/GameVoteSolution/Migrations/Configuration.cs
@@ -1,8 +1,6 @@
 namespace GameVoteSolution.Migrations
 {
     using GameVoteSolution.Models;
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -17,7 +15,43 @@ namespace GameVoteSolution.Migrations
 
         protected override void Seed(GameVoteSolution.Models.ApplicationDbContext context)
         {
-           
+            this.AddUserAndRoles();
         }
+
+        bool AddUserAndRoles()
+        {
+            bool success = false;
+            var idManager = new IdentityManager();
+            success = idManager.CreateRole("Admin");
+
+            if (!success == true) return success;
+            success = idManager.CreateRole("User");
+
+            if (!success) return success;
+
+
+            var newUser = new ApplicationUser()
+            {
+                UserName = "Admin",
+                Email = "Admin@hotornot.com"
+            };
+
+            // Be careful here - you  will need to use a password which will 
+            // be valid under the password rules for the application, 
+            // or the process will abort:
+
+            success = idManager.CreateUser(newUser, "Password1");
+            if (!success) return success;
+
+            success = idManager.AddUserToRole(newUser.Id, "Admin");
+            if (!success) return success;
+
+            success = idManager.AddUserToRole(newUser.Id, "User");
+            if (!success) return success;
+
+            return success;
+
+        }
+
     }
 }
